fd8e89833642f7022d3c8f9602e306fd76906af3
Philip Johnson <johnson@hawaii.edu>
2015-06-09 12:51:40 -1000

    now with json maybe working?

--- module-info.js
+++ module-info.js
@@ -1,9 +1,9 @@
 ics311 = {
 modules: {
-  { course: "ics311", name: "Learn to Foo", moduleUrl: "/basic-template2/modules/Learn to Foo" },
-  { course: "ics311", name: "Learn to Bar", moduleUrl: "/basic-template2/modules/Learn to Bar" },
-  { course: "ics311", name: "Learn to Baz", moduleUrl: "/basic-template2/modules/Learn to Baz" },
-  { course: "ics311", name: "Learn to Qux", moduleUrl: "/basic-template2/modules/Learn to Qux" }
+  { course: "ics311", name: "Learn to Foo", moduleUrl: "/basic-template2/modules/foo" },
+  { course: "ics311", name: "Learn to Bar", moduleUrl: "/basic-template2/modules/bar" },
+  { course: "ics311", name: "Learn to Baz", moduleUrl: "/basic-template2/modules/baz" },
+  { course: "ics311", name: "Learn to Qux", moduleUrl: "/basic-template2/modules/qux" }
 },
  prerequisites: {
   { moduleUrl: "/morea/bar/module.html", prerequisiteUrl: "/morea/bar/prerequisite-foo.html" }
