db72b9c167bb6ec2aa622f134fdc67613ff735ae
Philip Johnson <johnson@hawaii.edu>
2015-06-09 12:47:03 -1000

    now with json again

--- src/module-info.js
+++ src/module-info.js
@@ -1,9 +1,9 @@
 ics311 = {
 modules: {
-  { course: "ics311", name: "Learn to Foo", url: "/basic-template2//morea/foo/module.html" },
-  { course: "ics311", name: "Learn to Bar", url: "/basic-template2//morea/bar/module.html" },
-  { course: "ics311", name: "Learn to Baz", url: "/basic-template2//morea/baz/module.html" },
-  { course: "ics311", name: "Learn to Qux", url: "/basic-template2//morea/qux/module.html" }
+  { course: "ics311", name: "Learn to Foo", url: "/basic-template2/morea/foo/module.html" },
+  { course: "ics311", name: "Learn to Bar", url: "/basic-template2/morea/bar/module.html" },
+  { course: "ics311", name: "Learn to Baz", url: "/basic-template2/morea/baz/module.html" },
+  { course: "ics311", name: "Learn to Qux", url: "/basic-template2/morea/qux/module.html" }
 },
  prerequisites: {
   { moduleUrl: "/morea/bar/module.html", prerequisiteUrl: "/morea/bar/prerequisite-foo.html" }
