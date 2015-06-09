2516732991e48e66cc12f89f07b4abdab117f33a
Philip Johnson <johnson@hawaii.edu>
2015-06-09 12:37:29 -1000

    now with json

---
+++ module-info.js
@@ -0,0 +1,11 @@
+ics311 = {
+modules: {
+  { course: "ics311", name: "Learn to Foo", url: "/morea/foo/module.html" },
+  { course: "ics311", name: "Learn to Bar", url: "/morea/bar/module.html" },
+  { course: "ics311", name: "Learn to Baz", url: "/morea/baz/module.html" },
+  { course: "ics311", name: "Learn to Qux", url: "/morea/qux/module.html" }
+},
+ prerequisites: {
+  { moduleUrl: "/morea/bar/module.html", prerequisiteUrl: "/morea/bar/prerequisite-foo.html" }
+}
+}
