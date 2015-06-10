cf02ffdf68fad3fd40826efb3d37c6d60cfe7163
Philip Johnson <johnson@hawaii.edu>
2015-06-09 14:13:55 -1000

    still working on module-info.js

--- src/_site/module-info.js
+++ src/_site/module-info.js
@@ -1,11 +1,11 @@
 ics311 = {
 modules: {
-  { course: "ics311", name: "Learn to Foo", moduleUrl: "/modules/foo" },
-  { course: "ics311", name: "Learn to Bar", moduleUrl: "/modules/bar" },
-  { course: "ics311", name: "Learn to Baz", moduleUrl: "/modules/baz" },
-  { course: "ics311", name: "Learn to Qux", moduleUrl: "/modules/qux" }
+  { course: "ics311", title: "Learn to Foo", moduleUrl: "http://morea-framework.github.io/modules/foo", sort_order: 1, description: "This is the summary for Foo" },
+  { course: "ics311", title: "Learn to Bar", moduleUrl: "http://morea-framework.github.io/modules/bar", sort_order: 2, description: "How to bar." },
+  { course: "ics311", title: "Learn to Baz", moduleUrl: "http://morea-framework.github.io/modules/baz", sort_order: 3, description: "<p>This is the third module in this sample template.</p>\n" },
+  { course: "ics311", title: "Learn to Qux", moduleUrl: "http://morea-framework.github.io/modules/qux", sort_order: 4, description: "<p>This is an example module without any content.</p>\n" }
 },
  prerequisites: {
-  { moduleUrl: "/modules/bar", prerequisiteUrl: "http://cnn.com" }
+  { moduleUrl: "http://morea-framework.github.io/modules/bar", prerequisiteUrl: "http://cnn.com" }
 }
 }
