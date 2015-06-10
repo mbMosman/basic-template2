335af5a9f59ef43e1fe1d4f611e60a151e795076
Philip Johnson <johnson@hawaii.edu>
2015-06-09 14:01:33 -1000

    still working on module-info.js

--- module-info.js
+++ module-info.js
@@ -1,11 +1,11 @@
 ics311 = {
 modules: {
-  { course: "ics311", name: "Learn to Foo", moduleUrl: "http://morea-framework.github.io//basic-template2/modules/foo" },
-  { course: "ics311", name: "Learn to Bar", moduleUrl: "http://morea-framework.github.io//basic-template2/modules/bar" },
-  { course: "ics311", name: "Learn to Baz", moduleUrl: "http://morea-framework.github.io//basic-template2/modules/baz" },
-  { course: "ics311", name: "Learn to Qux", moduleUrl: "http://morea-framework.github.io//basic-template2/modules/qux" }
+  { course: "ics311", name: "Learn to Foo", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/foo" },
+  { course: "ics311", name: "Learn to Bar", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/bar" },
+  { course: "ics311", name: "Learn to Baz", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/baz" },
+  { course: "ics311", name: "Learn to Qux", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/qux" }
 },
  prerequisites: {
-  { moduleUrl: "http://morea-framework.github.io//basic-template2/modules/bar", prerequisiteUrl: "http://cnn.com" }
+  { moduleUrl: "http://morea-framework.github.io/basic-template2/modules/bar", prerequisiteUrl: "http://cnn.com" }
 }
 }
