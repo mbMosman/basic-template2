2f223f4a44a90ba7a20f75c114548a2679e6e983
Philip Johnson <johnson@hawaii.edu>
2015-06-09 14:12:20 -1000

    still working on module-info.js

--- module-info.js
+++ module-info.js
@@ -1,9 +1,9 @@
 ics311 = {
 modules: {
-  { course: "ics311", title: "Learn to Foo", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/foo", description: "This is the summary for Foo" },
-  { course: "ics311", title: "Learn to Bar", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/bar", description: "How to bar." },
-  { course: "ics311", title: "Learn to Baz", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/baz", description: "<p>This is the third module in this sample template.</p>\n" },
-  { course: "ics311", title: "Learn to Qux", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/qux", description: "<p>This is an example module without any content.</p>\n" }
+  { course: "ics311", title: "Learn to Foo", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/foo", sort_order: 1, description: "This is the summary for Foo" },
+  { course: "ics311", title: "Learn to Bar", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/bar", sort_order: 2, description: "How to bar." },
+  { course: "ics311", title: "Learn to Baz", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/baz", sort_order: 3, description: "<p>This is the third module in this sample template.</p>\n" },
+  { course: "ics311", title: "Learn to Qux", moduleUrl: "http://morea-framework.github.io/basic-template2/modules/qux", sort_order: 4, description: "<p>This is an example module without any content.</p>\n" }
 },
  prerequisites: {
   { moduleUrl: "http://morea-framework.github.io/basic-template2/modules/bar", prerequisiteUrl: "http://cnn.com" }
